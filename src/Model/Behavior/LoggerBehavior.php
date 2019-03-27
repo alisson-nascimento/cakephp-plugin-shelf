f10834d377144db1df7dbb9acd9a8ced6e539979
Alisson Nascimento <alisson.nascimento@gonubetec.com.br>
2019-03-27 11:41:27 -0300

    logs

--- src/Model/Behavior/LoggerBehavior.php
+++ src/Model/Behavior/LoggerBehavior.php
@@ -6,75 +6,82 @@ use Cake\ORM\Behavior;
 use Cake\Event\Event;
 use Cake\ORM\Entity;
 
-class LoggerBehavior extends Behavior {
+class LoggerBehavior extends Behavior
+{
 
-    public function initialize(array $config) {
+    public function initialize(array $config)
+    {
         parent::initialize($config);
     }
-    
-    private function arrayRecursiveDiff($aArray1, $aArray2) {
+
+    private function arrayRecursiveDiff($aArray1, $aArray2)
+    {
         $aReturn = array();
 
         foreach ($aArray1 as $mKey => $mValue) {
-          if (array_key_exists($mKey, $aArray2)) {
-            if (is_array($mValue)) {
-              $aRecursiveDiff = $this->arrayRecursiveDiff($mValue, $aArray2[$mKey]);
-              if (count($aRecursiveDiff)) { $aReturn[$mKey] = $aRecursiveDiff; }
+            if (array_key_exists($mKey, $aArray2)) {
+                if (is_array($mValue)) {
+                    $aRecursiveDiff = $this->arrayRecursiveDiff($mValue, $aArray2[$mKey]);
+                    if (count($aRecursiveDiff)) {
+                        $aReturn[$mKey] = $aRecursiveDiff;
+                    }
+                } else {
+                    if ($mValue != $aArray2[$mKey]) {
+                        $aReturn[$mKey] = $mValue;
+                    }
+                }
             } else {
-              if ($mValue != $aArray2[$mKey]) {
                 $aReturn[$mKey] = $mValue;
-              }
             }
-          } else {
-            $aReturn[$mKey] = $mValue;
-          }
         }
         return $aReturn;
-    } 
+    }
 
-    public function afterSave(Event $event, Entity $entity, \ArrayObject $options) {
-        
+    public function afterSave(Event $event, Entity $entity, \ArrayObject $options)
+    {
+
         $class = get_class($entity);
-        
-        if(!in_array($class, ['Shelf\\Model\\Entity\\ShelfLogRegistro', 'Shelf\\Entity\\Table\\ShelfLogDetalhe'])){
-            
+
+        if (!in_array($class, ['Shelf\\Model\\Entity\\ShelfLogRegistro', 'Shelf\\Entity\\Table\\ShelfLogDetalhe'])) {
+
             $registroTable = \Cake\ORM\TableRegistry::get('Shelf.ShelfLogRegistro');
             $logDetalheTable = \Cake\ORM\TableRegistry::get('Shelf.ShelfLogDetalhe');
             $log = $logDetalheTable->newEntity();
-            
+
             // pr($registroTable ); exit;
 
             $data = \Cake\Routing\Router::getRequest()->getData();
             unset($data['_save']);
-            if(empty($data)){
+            if (empty($data)) {
                 $data = $entity->toArray();
             }
-            
+
             $diff = $entity->extractOriginalChanged(array_keys($data));
-            
-//                $diff = $this->arrayRecursiveDiff($original, $data);
+
+            //                $diff = $this->arrayRecursiveDiff($original, $data);
 
             $query = $registroTable->find('all')
-            ->where(['modelo_pk'=>$entity->id, 'modelo_table'=>$class]);
+                ->where(['modelo_pk' => $entity->id, 'modelo_table' => $class]);
 
             $registro = $query->first();
-            
-            if($entity->isNew() || is_null($registro)){
+
+            if ( $entity->isNew() || is_null($registro)) {
 
                 $registro = $registroTable->newEntity();
 
                 $registro->modelo_table = $class;
                 $registro->created = date('Y-m-d H:i:s');
-                
+                $registro->created_by = $this->request->getSession()->read('Auth.User.id');
             }
             // pr($registro);exit;
-            if($entity->isNew()){
+            if ($entity->isNew()) {
                 $log->tipo_acao = 'Insert';
-            }else{
+            } else {
                 $log->dados_antigos = json_encode($diff);
-                $log->tipo_acao = 'Update';
+                $log->tipo_acao = 'Update';                
             }
 
+            $registro->created_by = $log->updated_by = $this->request->getSession()->read('Auth.User.id');
             $log->created = date('Y-m-d H:i:s');
 
             if ($registroTable->save($registro)) {
@@ -84,32 +91,33 @@ class LoggerBehavior extends Behavior {
                 // pr($log);exit;
                 $logDetalheTable->save($log);
             }
-        }                        
-    }    
-    
-    public function afterDelete(Event $event, Entity $entity, \ArrayObject $options) {
-         
+        }
+    }
+
+    public function afterDelete(Event $event, Entity $entity, \ArrayObject $options)
+    {
+
         $class = get_class($entity);
-        
-        if(!in_array($class, ['Shelf\\Model\\Entity\\ShelfRegistro', 'Shelf\\Model\\Entity\\ShelfLog'])){
+
+        if (!in_array($class, ['Shelf\\Model\\Entity\\ShelfRegistro', 'Shelf\\Model\\Entity\\ShelfLog'])) {
             $registroTable = \Cake\ORM\TableRegistry::get('Shelf.ShelfLogRegistro');
             $logDetalheTable = \Cake\ORM\TableRegistry::get('Shelf.ShelfLogDetalhe');
             $log = $logDetalheTable->newEntity();
 
             $query = $registroTable->find('all')
-            ->where(['modelo_pk'=>$entity->id, 'modelo_table'=>$class]);
+                ->where(['modelo_pk' => $entity->id, 'modelo_table' => $class]);
 
             $registro = $query->first();
 
             $log->tipo_acao = 'Delete';
             $log->created = date('Y-m-d H:i:s');
-            
+
             $registro_id = $registro->id;
             $log->shelf_log_registro_id = $registro_id;
 
-            if ($logDetalheTable->save($log)) {                
-               //TODO 
+            if ($logDetalheTable->save($log)) {
+                //TODO 
             }
-        }                
+        }
     }
 }
