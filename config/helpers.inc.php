0e642b5975179bd93d6dcf3927982e1071860556
Alisson Nascimento <alisson.nascimento@gonubetec.com.br>
2019-07-18 16:05:00 -0300

    namespaces nas funcoes e helpers

--- config/helpers.inc.php
+++ config/helpers.inc.php
@@ -1,14 +1,49 @@
 <?php
+use Shelf\Lib\Helper\L10n;
 
-/**
- * 
- * @return \Shelf\Lib\Shelf
- */
-function shelf()
-{
-    if(!is_null(\Shelf\Lib\Shelf::getInstance())){
-        return \Shelf\Lib\Shelf::getInstance();
+namespace shelf{
+    /**
+     * Undocumented function
+     *
+     * @return \Shelf\Lib\Helper\L10n
+     */
+    function l10n(){
+        if(!is_null(L10n::getInstance())){
+            return L10n::getInstance();
+        }
+        return new L10n();
     }
-    return new \Shelf\Lib\Shelf();
 }
 
+namespace shelf\icon{
+    /**
+     * Undocumented function
+     *
+     * @return \Shelf\Lib\Helper\Icon
+     */
+    function i($class){
+        return \phacil\html\i()->class($class);
+    }
+
+    /**
+     * Load Font Awesome Icon
+     * 
+     * @param String $icon
+     * @return String
+     */
+    function fa($icon)
+    {
+        return i('fa fa-' .$icon);
+    }
+
+    /**
+     * Load Pi Icon
+     * 
+     * @param String $icon
+     * @return String
+     */
+    function pi($icon)
+    {
+        return i('pi-' .$icon);
+    }
+}
